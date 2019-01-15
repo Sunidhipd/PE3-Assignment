7c93d1f02caf487231d1e16ca75ebc5c3ba9121c
Sunidhi Prasad <sunidhi15995@gmail.com>
2019-01-15 09:30:04 +0530

    Update RemoveVowelsFromPlaceArray.java

--- main/java/RemoveVowelsFromPlaceArray.java
+++ main/java/RemoveVowelsFromPlaceArray.java
@@ -3,30 +3,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class RemoveVowelsFromPlaceArray {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        String placeName = "";
-        List<String> places = new ArrayList<String>();
-        int n = sc.nextInt();
 
-        for (int i = 0; i < n; i++) {
-            placeName = sc.next();
-            places.add(placeName);
-        }
-
-        List<String> NameWithoutVowels = new ArrayList<String>();
-        NameWithoutVowels=VowelsRemoved(places);
-        System.out.println(NameWithoutVowels);
-
-        int j = 0;
-        for (int i = 0; i < NameWithoutVowels.size(); i++) {
-            System.out.println(" Place Name without Vowels:" + i + " " + NameWithoutVowels.get(i));
-            j++;
-        }
-
-    }
-
-    public static List<String> VowelsRemoved(List<String> place) {
+    public List<String> VowelsRemoved(List<String> place) {
         List<String> PlaceWithoutVowels = new ArrayList<String>();
         String placeName = "";
         for (int i = 0; i < place.size(); i++) {
@@ -36,9 +14,6 @@ public class RemoveVowelsFromPlaceArray {
         }
         return PlaceWithoutVowels;
     }
-
-
-
 }
 
 
