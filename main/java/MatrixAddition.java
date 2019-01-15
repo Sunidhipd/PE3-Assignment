36310afe3addbbc9cb5d122d8080280d249d97b3
Sunidhi Prasad <sunidhi15995@gmail.com>
2019-01-15 09:29:17 +0530

    Update MatrixAddition.java

--- main/java/MatrixAddition.java
+++ main/java/MatrixAddition.java
@@ -4,15 +4,11 @@ public class MatrixAddition {
    
     public int[][] sum(int firstMatrix[][], int secondMatrix[][],int rows,int columns){
             int sumMatrix[][] = new int[rows][columns];
-
-
             for(int i=0;i<rows;i++){
             for(int j=0;j<columns;j++){
                 sumMatrix[i][j]=firstMatrix[i][j]+secondMatrix[i][j];
             }
         }
-
         return sumMatrix;
-
     }
 }
