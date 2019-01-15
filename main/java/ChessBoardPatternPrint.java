1e7287537fa9e4b6ffa19333430f1dbced4534b7
Sunidhi Prasad <sunidhi15995@gmail.com>
2019-01-15 09:27:02 +0530

    Update ChessBoardPatternPrint.java

--- main/java/ChessBoardPatternPrint.java
+++ main/java/ChessBoardPatternPrint.java
@@ -1,11 +1,8 @@
 import java.util.Scanner;
 
 public class ChessBoardPatternPrint {
-    public static void main(String args[]){
-    printChessBoard();
-
-}
-    public static void printChessBoard(){
+   
+    public void printChessBoard(){
         for(int i=0;i<8;i++){
             for(int j=0;j<8;j++) {
                 if (i % 2 == 0) {
