95f347844f5050458dab7c52648d25515f543e1c
Sunidhi Prasad <sunidhi15995@gmail.com>
2019-01-15 09:26:29 +0530

    Update CheckConsecutiveNumbers.java

--- main/java/CheckConsecutiveNumbers.java
+++ main/java/CheckConsecutiveNumbers.java
@@ -3,22 +3,7 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class CheckConsecutiveNumbers {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        Integer numberArray[]=new Integer[7];
-        int n;
-        for(int i=0;i<7;i++) {
-            n = sc.nextInt();
-            numberArray[i]=n;
-        }
-        boolean res=CheckConsecutive(numberArray);
-        if(res==true)
-        System.out.println("consecutive");
-        else
-            System.out.println("Not consecutive");
-    }
-
-    public static boolean CheckConsecutive(Integer numberArray[]) {
+    public boolean CheckConsecutive(Integer numberArray[]) {
         String res = "";
         int min = Collections.min(Arrays.asList(numberArray));
         int max = Collections.max(Arrays.asList(numberArray));
