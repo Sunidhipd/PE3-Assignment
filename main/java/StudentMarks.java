bc44d573e2b1e45107296d172b97ffcc2ce04699
Sunidhi Prasad <sunidhi15995@gmail.com>
2019-01-15 09:31:30 +0530

    Update StudentMarks.java

--- main/java/StudentMarks.java
+++ main/java/StudentMarks.java
@@ -1,26 +1,8 @@
 import java.util.*;
 
 public class StudentMarks {
-    public static void main(String args[]) {
-        System.out.print("Input the number of Students: ");
-        int numOfStudents;
-        int marks;
-        Scanner sc = new Scanner(System.in);
-        numOfStudents = sc.nextInt();
-        Scanner scan = new Scanner(System.in);
-        int stuGrades[] = new int[numOfStudents];
-        for (int i = 0; i < numOfStudents; i++) {
-            System.out.print("Enter marks for Student with roll number " + (i + 1) + ": ");
-            marks = sc.nextInt();
-            stuGrades[i] = marks;
-        }
-            boolean check = CheckGrades(numOfStudents,stuGrades);
-            if (check == false) System.out.println("ERROR! INVALID INPUT");
-            else System.out.println("All good");
-    }
-
-    public static boolean CheckGrades(int numOfStudents,int[] stuGrades) {
-
+    
+    public boolean CheckGrades(int numOfStudents,int[] stuGrades) {
             if(numOfStudents==0){
                 return false;
             }
@@ -32,6 +14,5 @@ public class StudentMarks {
                 }
                 return true;
             }
-
     }
 }
